412547366342d93477767ff011290a8102eb0b95
davidgonvis <galvisd153@gmail.com>
2021-06-12 05:16:10 +0800

    design

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container } from '@material-ui/core';
+import InputGroup from './components/InputGroup';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <InputGroup group_id="1"></InputGroup>
+      <InputGroup group_id="2"></InputGroup>
+      <InputGroup group_id="3"></InputGroup>
+      <InputGroup group_id="4"></InputGroup>
+      <InputGroup group_id="5"></InputGroup>
+    </Container>
   );
 }
 
